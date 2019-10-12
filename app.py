678de798a33a26bc7581befdf906fd842819a01d
David Alley <dpalley@gmail.com>
2019-10-12 12:23:03 -0500

    Add bootstrap and navigation, add routes, fix sticky footer

--- app.py
+++ app.py
@@ -9,16 +9,23 @@ def get_teams():
         # return json.loads(f)
         return teams
 
-
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title="Home")
+    return render_template('home.html')
 
 @app.route('/teams')
 def teams():
     teams = get_teams()
     return render_template('teams.html', title="Teams", teams=teams)
+
+@app.route('/players')
+def players():
+    return render_template('players.html', title="Players")
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title="Login")
 
 if __name__ == '__main__':
     app.run(debug=True)
